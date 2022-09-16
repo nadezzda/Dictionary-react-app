d0a63cdf64a8d5eb3eaf0cfaaaa76b22386cc62b
nadezzda <malicenkonadic@gmail.com>
2022-09-16 18:22:20 +0300

    Add some style

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Example from "./Example";
 
 export default function Meaning(props){
     return (
@@ -8,10 +9,9 @@ export default function Meaning(props){
             return (
                 <div key={index}>
                     <p>
-                        <strong>Definition:</strong>{definition.definition}
+                        <strong>Definition: </strong>{definition.definition}
                         <br />
-                        <strong>Example:</strong>
-                        <em>{definition.example}</em>
+                        <Example example={definition.example} />
                     </p>
                 </div>
             );
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -6,7 +6,11 @@ export default function Synonyms(props) {
             <div>
             <ul className="Synonyms">
                 {props.synonyms.map(function(synonym, index) {
-                    return <li key={index}>{synonym}</li>;
+                    if (index < 5) {
+                        return <li key={index}>{synonym}</li>;
+                    } else {
+                        return null;
+                    }
                 })}
             </ul>
             </div>

b273b379bd792094c251cfdb3d287cc4100ae41c
nadezzda <malicenkonadic@gmail.com>
2022-09-17 20:34:39 +0300

    Add displaying photos

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,32 @@
 import React, {useState} from "react";
 import "./Search.css";
 import Result from "./Result";
+import Photos from "./Photos";
 import axios from "axios";
 
 export default function SearchEngine(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
-    let [loaded, setLoaded] = useState(false)
+    let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response){
+    function handleDictionaryResponse(response){
         setResults(response.data[0]);
+    }
+
+    function handlePexelsResponse(response) {
+        setPhotos(response.data.photos);
     }
 
     function search() {
         //documentation: https://dictionaryapi.dev/
         let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+        //documentation: https://www.pexels.com/ru-ru/api/documentation/?
+        let pexelsApiKey = `563492ad6f91700001000001467b24b70ec44ba0887bda4994131f33`;
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+        let headers = {"Authorization" : `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsResponse);
     }
 
     function handeSubmit(event) {
@@ -44,6 +55,7 @@ export default function SearchEngine(props) {
                 </section>
                 </div>
                 <Result results={results} />
+                <Photos photos={photos} />
         </div> 
     );
     } else {
